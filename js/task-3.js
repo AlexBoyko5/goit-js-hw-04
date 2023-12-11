866d9d0ce223ef5e2e3ac6a171c507d38763a0f2
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2023-12-11 17:55:45 +0100

    commitTask3

--- js/task-3.js
+++ js/task-3.js
@@ -6,7 +6,6 @@
 //     username: "Jacob",
 //     playTime: 300,
 // };
-
 // Доповни об’єкт profile методами для роботи з його властивостями.
 // Метод changeUsername(newName) повинен приймати рядок(нове ім’я) в параметр newName
 // та змінювати значення властивості username на нове.Нічого не повертає.
@@ -16,41 +15,28 @@
 //     < Username > has < amount > active hours!, де < Username > — це ім’я профілю,
 //     а < amount > — кількість ігрових годин.
 
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
-// function filterArray(numbers, value) {//* Оголошуємо функцію*/
+    changeUsername(newName) { //* Метод для зміни імені 
+        this.username = newName;
+    },
 
-//   //* Створюємо порожній масив для чисел/
-//   let resultArray = [];
+    updatePlayTime(hours) { //* Метод для оновлення кількості активних годин
+        this.playTime += hours;
+    },
 
-//   //* Проходимо циклом по кожнму елементу масиву/
-//   for (let i = 0; i < numbers.length; i++) {
-
-//     //*Перевірка, чи поточний елемент більший за задане значення
-//     if (numbers[i] > value) {
-
-//       //* Додаємо поточне число до масиву
-//       resultArray.push(numbers[i]);
-//     }
-//   }
-//   //* Повертаємо масив чисел*/
-//   return resultArray;
-// }
-
-// //~ (i < numbers.length;) каже, що ми хочемо ітерувати кожен елемент масиву numbers, а не порівнювати його зі значенням value.
-// //~Умова (i < numbers.length) означає, що цикл буде продовжуватися, доки i менше довжини масиву numbers.
-// //~ Це дозволяє нам ітерувати кожен елемент масиву, незалежно від його значення.
-// //~ якщо написати (for (let i = 0; i > value; i++) -(числа numbers, які більші за значення value)=>
-// //~замість (for (let i = 0; i < numbers.length; i++))
-// //~ то цикл не буде ітерувати кожен елемент масиву numbers, а буде перевіряти, чи є значення i більшим за value.
-// //~Це може призвести до того, що цикл не буде виконуватися жодного разу, якщо value більше, ніж довжина масиву numbers.
-// //~Якщо value менше, ніж довжина масиву numbers, то цикл буде виконуватися, але не для кожного елемента масиву numbers.
+    getInfo() { //* отримання інформації про профіль
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
 
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
