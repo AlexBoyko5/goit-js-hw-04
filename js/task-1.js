0214b2f6d52e3399dab97619c5d90076321612aa
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2023-12-17 12:26:13 +0100

    update-task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,25 +1,23 @@
 // Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
-
 // Функція оголошує два параметри:
-
 // products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів.Наприклад, { apples: 2, grapes: 4 }.
-//     containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
 // Функція має повернути результат перевірки, чи помістяться всі товари в контейнер.Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
 
-// function isEnoughCapacity(products, containerSize) {
-//     // Перебираємо product через ключі об'єкта products за допомогою for...of
-//     for (const productName of Object.keys(products)) {
-//         const productQuantity = products[productName]; //  Отримуємо значення (значення властивості),
-//         // що відповідає ключу productName в об'єкті products
-//         // Якщо кількість товару більша за доступний об'єм контейнера, повертаємо false
-//         if (productQuantity > containerSize) {
-//             return false;
-//         }
-//     }
-//     // Якщо всі товари поміщаються в контейнер, повертаємо true
-//     return true;
-// }
+function isEnoughCapacity(products, containerSize) {
+    // Перебираємо product через ключі об'єкта products за допомогою for...of
+    for (const productName of Object.keys(products)) {
+        const productQuantity = products[productName]; //  Отримуємо значення (значення властивості),
+        // що відповідає ключу productName в об'єкті products
+        // Якщо кількість товару більша за доступний об'єм контейнера, повертаємо false
+        if (productQuantity > containerSize) {
+            return false;
+        }
+    }
+    // Якщо всі товари поміщаються в контейнер, повертаємо true
+    return true;
+}
 
 // якщо писати так,
 // if (productQuantity <= containerSize) {
